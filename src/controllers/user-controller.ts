096485d2f745f5735cb415656002415bdeff5f90
rajcrk <rajkumar.cind@gmail.com>
2019-02-10 18:15:07 +0530

    structure setup

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,26 @@
+import { Service, Container } from "typedi";
+import { JsonController, Post, Req, Res, Body, Get, Put, Param, Delete } from "routing-controllers";
+import { Response } from "express-serve-static-core";
+import { UserManagementDelegate } from "../delegates/user-delegate";
+import { AddUserRequest } from "../models/request-responses/add-user/addUserRequest";
+
+@Service()
+@JsonController("/users")
+export class UserManagementController {
+    userManagementDelegate: UserManagementDelegate;
+    constructor(){
+        // We use typedi to get the instance of UserManagementDelegate
+        this.userManagementDelegate = Container.get(UserManagementDelegate);
+    }
+
+    @Post("/")
+    async addUser(@Req req: Request, @Res res: Response, @Body addUserRequest: AddUserRequest): Promise<any> {
+        try {
+            const response = await this.userManagementDelegate.addUser(addUserRequest);
+            return res.send(new SuccessResponse(response));
+        } catch(err) {
+
+        }
+    }
+
+}
